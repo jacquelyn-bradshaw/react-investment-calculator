9085e219e18adb5d023f108a28ec8aebe0896dc5
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-04-19 14:02:57 +0100

    results component complete

--- src/Components/ResultsTable.jsx
+++ src/Components/ResultsTable.jsx
@@ -1,4 +1,4 @@
-import {calculateInvestmentResults } from "../util/investment.js"
+import {calculateInvestmentResults, formatter } from "../util/investment.js"
 
 export default function ResultsTable ({userInput}) {
   const calculatedInvestment = calculateInvestmentResults(userInput);
@@ -16,7 +16,22 @@ export default function ResultsTable ({userInput}) {
       </tr>
     </thead>
     <tbody>
-        {calculatedInvestment.map((year) => {<tr><td>year</td></tr>})}
+        {calculatedInvestment.map(year => {
+            const totalInterest = 
+              year.valueEndOfYear -
+              (year.annualInvestment * year.year) -
+              userInput.initialInvestment;
+
+            const capitalInvested = year.valueEndOfYear - totalInterest;
+
+          return <tr key={year.year}>
+            <td>{year.year}</td>
+            <td>{formatter.format(year.valueEndOfYear)}</td>
+            <td>{formatter.format(year.interest)}</td>
+            <td>{formatter.format(totalInterest)}</td>
+            <td>{capitalInvested}</td>
+          </tr>
+        })}
     </tbody>
   </table>
 }
